f9948224c677b3c7cfdcd15c2f39d5ea53534f75
fluttervieh <lorenz.richard@gmx.at>
2022-11-24 21:48:13 +0100

    messed a little bit around

---
+++ lib/ModelClasses/StoreItem.dart
@@ -0,0 +1,19 @@
+class StoreItem{
+  final String identifier;
+  final String url;
+  final String username;
+  final String password;
+  final List<String> tags;
+
+
+  const StoreItem(
+    this.identifier, 
+    this.url, 
+    this.username, 
+    this.password, 
+    this.tags
+  );
+
+  
+
+}
